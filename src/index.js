dcf72c9733bf39f70489e5be07eb8e56ef707612
tylersemel <tylersemel@gmail.com>
2025-01-22 14:44:57 -0500

    Create module to load the general project page

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
 import { todoItemManager } from "./todo-item-manager.js";
 import { createProject } from "./project-module.js";
+import { generalPageLoader } from "./general-page-loader.js";
 import "./styles.css";
 
-let projectsList = [];
-const generalProject = createProject('General', 'Tasks with no project goes here!');
-projectsList.push(generalProject);
 
-let todo1 = todoItemManager.createTodoItem('Go to the store', 'Buy milk, eggs, beef', '01-21-2025', '', 'DOING');
-let todo2 = todoItemManager.createTodoItem('Eat grocercies');
-let todo3 = todoItemManager.createTodoItem('Have fun');
+document.addEventListener('DOMContentLoaded', () => {
+    generalPageLoader.loadPage();
+})
 
 
-let myProject = createProject('tite');
-let myProject2 = createProject('2');
 
-myProject.addTodoToList(todo1);
-myProject.addTodoToList(todo2);
-myProject.addTodoToList(todo3);
 
-myProject2.addTodoToList(todo2);
 
-myProject.removeTodoFromList(todo2);
 
-myProject.printAllTodos();
+
+
+
+// let projectsList = [];
+// const generalProject = createProject('General', 'Tasks with no project goes here!');
+// projectsList.push(generalProject);
+
+// let todo1 = todoItemManager.createTodoItem('Go to the store', 'Buy milk, eggs, beef', '01-21-2025', '', 'DOING');
+// let todo2 = todoItemManager.createTodoItem('Eat grocercies');
+// let todo3 = todoItemManager.createTodoItem('Have fun');
+
+
+// let myProject = createProject('tite');
+// let myProject2 = createProject('2');
+
+// myProject.addTodoToList(todo1);
+// myProject.addTodoToList(todo2);
+// myProject.addTodoToList(todo3);
+
+// myProject2.addTodoToList(todo2);
+
+// myProject.removeTodoFromList(todo2);
+
+// myProject.printAllTodos();
